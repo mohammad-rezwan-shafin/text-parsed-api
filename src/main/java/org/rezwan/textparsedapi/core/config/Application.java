7f1b33729e07bac3273ad0cd3535f719ba6d2d2d
shafin <shafin@shafin-linux-pc.gateway>
2017-06-07 13:45:04 +1000

    Added RestTemplate and Commponet Scan to Endpoint RestControllers

--- src/main/java/org/rezwan/textparsedapi/core/config/Application.java
+++ src/main/java/org/rezwan/textparsedapi/core/config/Application.java
@@ -1,12 +1,21 @@
 package org.rezwan.textparsedapi.core.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 
 @Slf4j
@@ -16,11 +25,28 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(
         basePackages = {
             "org.rezwan.textparsedapi.core.service",
+            "org.rezwan.textparsedapi.api.endpoint",
         }
 )
 @EnableScheduling
 public class Application {
+    
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<HttpMessageConverter<?>> listConverters = new ArrayList<>();
+        listConverters.add(new MappingJackson2HttpMessageConverter());
+        listConverters.add(new MappingJackson2XmlHttpMessageConverter());
+
+        restTemplate.setMessageConverters(listConverters);
+        restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+
+        return restTemplate;
     }    
 }
